05fc291b792a891e191a1160c4c7e49956c88234
Qyubee99 <kim4188123@naver.com>
2022-10-19 21:47:21 +0900

    22.10.19(참조자)

--- ajhC++.cpp
+++ ajhC++.cpp
@@ -1,117 +1,118 @@
 #include <iostream> 
-#include <string> 
 using namespace std; 
 
-// 이름 공간(namespace)이란?
+// 레퍼런스 연산자의 경우
 /*
-// 속성을 구분할 수 있도록 유효 범위를 설정하는 영역입니다.
+// 포인터를 이용하여 연산을 허용하지 않습니다.
 
-namespace ClientA
+// 참조자를 사용하는 이유
+// 
+void Function(int & x) // <- 1000 mb
 {
-    void Attack()
-    {
-        cout << "원거리 공격" << endl;
-    }
+    x = 100;
 }
-// ClientA의 이름 공간을 명시하지 않아도 사용하겠다는 의미입니다.
-using namespace ClientA;
-
-namespace ClientB
-{
-    void Attack()
-    {
-        cout << "근거리 공격" << endl;
-    }
-}
-
-int value = 20; // 전역 변수 value
-                // 데이터 영역
 */
 
-// 기본 매개변수
+// 인라인 함수
 /*
-// 함수를 선언할 때 매개변수를 선언과 동시에 초기화하는 매개변수입니다.
-// 기본 매개변수는 오른쪽에서 부터 정의해야 합니다.
-void Function(int x, int y = 30, int z = 30)
+// 함수를 호출하는 대신 함수가 호출되는 위치마다
+// 함수의 코드를 복사하여 전달하는 방식의 함수입니다.
+inline void Function()
 {
-    cout << "x의 값 : " << x << endl;
-    cout << "y의 값 : " << y << endl;
+    cout << "공격" << endl;
 }
+*/
+
+// 캡슐화
+/*
+class cellPhone
+{
+private:
+    int brightness;
+    int volume;
+
+    bool power;
+
+public:
+    // 값을 접근하는 함수(Setter)
+    // 값을 읽는 함수 (Getter)
+    
+    // Setter() 함수
+    void SetVolume(int x)
+    {
+        volume = x;
+    }
+    // Getter() 함수
+    int GetVolume()
+    {
+        return volume;
+    }
+
+    void SoundVolume(int volumeValue)
+    {
+        if (volumeValue >= 100)
+        {
+            volumeValue = 100;
+        }
+        else if (volumeValue < 0)
+        {
+            volumeValue = 0;
+        }
+        volume = volumeValue;
+
+        cout << volume << endl;
+    }
+    
+
+};
 */
 
 int main()
 {
-    // 범위 지정 연산자란?
+    // 참조자란?
     /*
-    // 여러 범위에서 사용되는 식별자를 구별하는데 사용하는 연산자입니다.
+    // 자신이 참조하는 변수를 대신할 수 있는 또 하나의 이름입니다.
+    
+    // 하나의 메모리 공간에 2개의 이름이 존재하는 형태가 되는 것입니다.
+    int a = 10;
+    int & tvalue = a; 
+    int& ref = tvalue;
 
-    int value = 10; // 지역 변수 value
-                    // 스택 영역
-                    
-    // 컴퓨터 언어 이론(범위 규칙)
-    // 전역 변수와 같은 이름의 지역변수가 선언되었을 때
-    // 가장 가까운 범위에 존재하는 변수의 이름을 사용하는 규칙입니다.
+    // 참조자는 초기화를 하지 않으면 사용할 수 없습니다.
+    // 참조자는 NULL로도 초기화를 할 수 없습니다.
+    int b = 20;
 
-    cout << "value의 값 : "<< value << endl;
-    cout << "전역 변수 value의 값 : " << ::value << endl;
+    int & va = a;
+    va = b;
+    
+    cout << a << endl;
 
-    Attack(); // using namespace를 사용하기 때문에 
-              // ClientA:: <-
-    ClientB::Attack();
-    */
-
-    // 기본 매개변수
-    /*
-    // 기본 매개변수를 선언하게 되면 함수의 인수를 전달하지 않아도 사용할 수 있습니다.
-
-    // 함수에 인수를 전달할 때 매개변수의 왼쪽부터 값이 들어갑니다.
-    Function(5);
+    Function(a);
     */
     
-    // getline
+    // 인라인 함수
     /*
-    string name;
-    // [A][n][\0][j][i][\0][h][u][n]
-    // 문자열은 NULL문자까지만 출력합니다.
-
-    // cin >> name;
-
-    // getline() 함수 : 표준입력을 받는 경우 Enter의 여부에 따라
-    // 입력받을 문자열을 구분합니다.
-    getline(cin, name);
-
-    cout << name;
+    // 함수 안에 있는 내용이 간단할 때 사용해야 합니다.
+    // 함수가 여러 번 호출되는 함수일 경우 인라인 함수를 사용하면
+    // 컴파일 크기가 커지게 됩니다.
+    Function();
     */
 
-    // OX 퀴즈
-    int input, score, sum;
-    char buffer[80];
+    // 객체 지향 프로그래밍
+    // 여러 개의 객체들끼리 상호작용을 통해
+    // 로직을 구성하는 프로그래밍 방법입니다.
 
-    cin >> input;
 
-    for (int i = 0; i < input; i++)
-    {
-        sum = 0;
-        score = 1;
+    // 캡슐화
+    /*
+    // 클래스의 속성(변수)과 기능(함수)를 하나로 묶은 다음
+    // 실제 구현 내용의 일부를 내부에 감추어 은닉하는 함수입니다.
 
-        cin >> buffer;
-        
-        //[O][X][O][X][O]
-        for (int j = 0; j < strlen(buffer); j++)
-        {
-            if (buffer[j] == 'O')
-            {
-                sum += score;
-                score++;
-            }
-            if (buffer[j] != 'O')
-            {
-                score = 1;
-            }
-        }
-        cout << sum << endl;
-    }
+    cellPhone iphone;
+    iphone.SetVolume(10);
 
+    cout << iphone.GetVolume() << endl;
+    */
 
 
     return 0;
