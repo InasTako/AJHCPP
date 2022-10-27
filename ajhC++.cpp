97bc780d559ca314ee604ddcf079882e237d516b
Qyubee99 <kim4188123@naver.com>
2022-10-27 21:46:29 +0900

    22.10.27(스마트 포인터)

--- ajhC++.cpp
+++ ajhC++.cpp
@@ -1,129 +1,117 @@
 #include <iostream> 
-#include <conio.h>
-#include <time.h>
+#include <memory>
 using namespace std; 
 
-// 가상 소멸자란?
+// 스마트 포인터란?
 /*
-// 가상으로 선언한 소멸자입니다.
-// 가상 소멸자는 존재하지만, 가상 생성자는 존재하지 않습니다.
+// 포인터처럼 동작하는 클래스 템플렛으로 사용이 끝난 메모리를
+// 자동으로 해제하는 포인터입니다.
 
-class A 
+class Player
 {
 public:
-    A()
+    Player()
     {
-        cout << "A클래스 생성" << endl;
+        cout << "Player 생성" << endl;
     }
-    virtual ~A()
+    ~Player()
     {
-        cout << "A클래스 소멸" << endl;
-    }
-};
-
-class B : public A
-{
-public:
-    B()
-    {
-        cout << "B클래스 생성" << endl;
-    }
-    virtual ~B()
-    {
-        cout << "B클래스 소멸" << endl;
+        cout << "Player 소멸" << endl;
     }
 };
 */
 
-// 프렌드
-/*
-// 클래스의 멤버 함수는 아니지만, 클래스에 friend라고 선언하게 되면
-// 클래스의 멤버 함수처럼 private 멤버에 접근할 수 있는 기능입니다. 
-
-class PeopleA
-{
-private:
-    int age;
-    // 친구로 선언할 클래스의 이름을 선언합니다.
-
-    friend void Information(PeopleA a) // <- 전역 함수로 선언
-    {
-        cout << "PeopleA 클래스 정보" << endl;
-    }
-
-    // friend 키워드는 public, private, protected
-    // 어디에서든 사용가능합니다.
-    friend class PeopleB;
-public:
-
-    PeopleA(int age)
-    {
-        this->age = age;
-    }
-};
-
-class PeopleB
+class Person
 {
 public:
-    void FriendInformation(PeopleA people)
+    // shared_ptr로 선언할 경우 순환 참조가 발생합니다.
+    // weak_ptr로 선언할 경우 순환 참조를 예방할 수 있습니다.
+    weak_ptr<Person> person;
+
+    Person()
     {
-        cout << people.age << endl;
+        cout << "생성" << endl;
+    }
+    ~Person()
+    {
+        cout << "소멸" << endl;
     }
 };
-*/
 
 int main()
 {
-    // 가상 소멸자
+    // shared_ptr 포인터
     /*
-    // 가상 소멸자가 호출되면 상속 구조의 맨 아래에 있는 하위 클래스의 
-    // 소멸자가 대신 호출되면서 상위 클래스의 소멸자가 순서대로 호출됩니다.
-    A * aptr = new B();
-    delete aptr;
+    // 어떤 하나의 객체를 참조하는 스마트 포인터 갯수를 참조하는 포인터입니다.
+    // 참조하고 있는 스마트 포인터의 갯수를 참조 카운트라고 합니다.
+    
+    // 참조 카운트란?
+    // 해당 메모리를 참조하는 포인터가 몇개가 있는지 나타내는 값입니다.
+    // 참조하는 포인터가 추가되면 1씩 증가하고 참조하는 포인터가 끊어지면 1씩 감소합니다.
+
+    // 첫번째로 초기화 하는 방법
+    // shared_ptr<int> ptr1 (new int(5));
+    // 두번째로 초기화 하는 방법
+    // shared_ptr<int> ptr1 = make_shared<int>(5);
+
+    // shared_ptr에서 참조 카운트를 확인하는 방법
+    // ptr1.use_count()
+
+    shared_ptr<Player> ptr1(new Player());
+    cout << "ptr1 참조 카운트 : " << ptr1.use_count() << endl;
+
+    shared_ptr<Player> ptr2 = ptr1;
+    cout << "ptr1 참조 카운트 : " << ptr2.use_count() << endl;
+
+    // shared_ptr<Player> ptr3(new Player());
+    // cout << "ptr3 참조 카운트 : " << ptr3.use_count() << endl;
     */
 
-    // 프렌드
+    // unique_ptr 포인터
     /*
-    PeopleA a(10);
-    Information(a);
-    PeopleB b;
-    b.FriendInformation(a);
+    // 하나의 스마트 포인터만 가리킬 수 있는 포인터입니다.
+    // shared_ptr 참조 카운트를 1 이상 넘길 수 없습니다.
+    
+    unique_ptr<Player> ptr1(new Player());
+
+    // ptr1이 가지고 있던 메모리 소유권(동적으로 할당한 메모리)를
+    // ptr2에게 메모리 소유권을 넘겨주는 과정입니다.
+
+    unique_ptr<Player> ptr2 = move(ptr1);
+
+    // reset : 메모리 영역을 삭제할 수 있는 함수
+    ptr2.reset();
+
+    // unique_ptr<int> ptr3 = ptr1;
     */
 
-    // 곱셈
+    // weak_ptr 포인터
     /*
-    int num1 = 0;
-    int num2 = 0;
+    // 하나 이상의 shared_ptr 인스턴스가 소유하는 객체에 대한 접근을 
+    // 제공하지만, 참조 카운트에 포함되지 않는 스마트 포인터입니다.
 
-    cin >> num1 >> num2;
-    cout << num2 % 10 * num1 << endl;
-    cout << num2 / 10 % 10 * num1 << endl;
-    cout << num2 / 100 % 10 * num1 << endl;
-    cout << num1 * num2 << endl;
+    // 만약에 서로가 상대를 가리키는 shared_ptr을 가지고 있다면,
+    // 참조 횟수는 절대 1 이하로 떨어지지 않습니다.
+    // shared_ptr이 해제가 되지 않는 문제가 발생합니다.
+
+    // 순환 참조
+    // 서로 상대를 참조하는 상황입니다.
+
+    // shared_ptr 인스턴스 사이에 순환 참조를 제거하기 위해 사용하는
+    // 스마트 포인터 입니다.
+
+    shared_ptr<Person> obj1(new Person());
+    shared_ptr<Person> obj2(new Person());
+
+    cout << "obj1 참조 카운트 : " << obj1.use_count() << endl;
+    cout << "obj2 참조 카운트 : " << obj2.use_count() << endl;
+
+    obj1->person = obj2;
+    obj2->person = obj1;
+
+    cout << "obj1 참조 카운트 : " << obj1.use_count() << endl;
+    cout << "obj2 참조 카운트 : " << obj2.use_count() << endl;
     */
-
-    // 중복되지 않는 랜덤값
-    // 1.배열을 생성합니다.
-    // 2.배열의 순차적으로 값을 저장합니다.
-
-    // 배열의 인덱스 == 배열의 인덱스 +1
-    int array[10];
-
-    srand(time(NULL));
-    for (int i = 0; i < 10; i++)
-    {
-        array[i] = rand() % 10+1;
-
-        if (array[i] == i)
-        {
-
-        }
-        else if (array[i] != i)
-        {
-
-        }
-        
-    }
 
     return 0;
 }
