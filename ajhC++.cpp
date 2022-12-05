86c513b6243c2202476dc5895959e5f18822c787
Qyubee99 <kim4188123@naver.com>
2022-12-05 21:48:32 +0900

    22.12.05(체이닝 기법)

--- ajhC++.cpp
+++ ajhC++.cpp
@@ -1,30 +1,70 @@
 #include<iostream>
 using namespace std;
 
+struct Node
+{
+    int key;
+    int value;
+    Node * next;
+};
+
+struct Bucket
+{
+    Node * head;
+    int size;
+};
+
+struct Bucket * hash;
+
+Node * CreatNode(int key, int value)
+{
+    Node * newnode = new Node;
+    newnode->key = key;
+    newnode->value = value;
+    newnode->next = NULL;
+
+    return newnode;
+}
+
+int HashFunction(int key)
+{
+    return key % 5;
+}
+
+void Insert(int key, int value)
+{
+    // hash[insex].size == 0
+    int hashindex = HashFunction(key);
+
+    // 방금 생성한 새로운 노드의 주소가 반환됩니다.
+    struct Node * newnode = CreatNode(key, value);
+
+    Bucket* hash = new Bucket[5];
+
+    // 버켓(hash)의 인덱스의 size가 0이라면 
+    if (hash[hashindex].size == 0)
+    {
+        // 사이즈값 증가
+        hash[hashindex].size++;
+
+        // head 포인터를 새로 처음 들어온 newnode를 가리키도록 합니다.
+        hash[hashindex].head = newnode;
+        
+    }
+    else
+    {
+
+    }
+
+}
 
 int main()
 {
-    // 계수 정렬
-    /*
-    // 데이터의 값을 직접 비교하지 않고, 단순하게 각 숫자가
-    // 몇 개 있는지 갯수를 세어 저장한 다음 정렬하는 알고리즘입니다.
+    // 해시 테이블
+    // 해시 함수를 사용해서 변환한 값을 index로삼아서
+    // key와 value를 저장하는 자료구조입니다.
+
     
-    int array[10] = {1, 5, 4, 1, 2, 4, 3, 3, 3, 5};
-    int count[5] = {0, };
-
-    for (int i = 0; i < 10; i++)
-    {
-        count[array[i] - 1] += 1;
-    }
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < count[i]; j++)
-        {
-            cout << i + 1 << endl;
-        }
-    }
-    */
-
 
     return 0;
 }
